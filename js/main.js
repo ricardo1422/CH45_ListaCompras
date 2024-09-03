146e9f5e190decac0b55f33df849d9f5b0796bf3
Padeon <padeon22@gmail.com>
2024-09-03 13:02:19 -0600

    Se añadio la funcion de obtener precio, la tabla de productos y almacenamiento en el localstore

--- js/main.js
+++ js/main.js
@@ -3,6 +3,17 @@ const txtNombre=document.getElementById("Name");
 const txtNumber=document.getElementById("Number");
 const alertValidaciones=document.getElementById("alertValidaciones");
 const alertValidacionesTexto=document.getElementById("alertValidacionesTexto");
+const tablaListaCompras=document.getElementById("tablaListaCompras");
+const cuerpoTabla=tablaListaCompras.getElementsByTagName("tbody").item(0);
+const contadorProductos= document.getElementById("contadorProductos");
+const precioTotal=document.getElementById("precioTotal")
+const productosTotal=document.getElementById("productosTotal")
+let isValid= true;
+let contador= 0;
+let precio=0;
+let costoTotal=0;
+let totalEnProductos=0;
+
 
 function validarCantidad()
 {
@@ -19,18 +30,24 @@ function validarCantidad()
     return true;
 }
 
+function getPrecio() {
+    return Math.round((Math.random()*10000))/100;
+}
+
 btnAgregar.addEventListener("click", function(event){
     event.preventDefault();
     txtNombre.style.border="";
     txtNumber.style.border="";
     alertValidaciones.style.display="none"
     alertValidacionesTexto.innerHTML="";
+    isValid=true;
     //valida nombre del producto
     if(txtNombre.value.length<3)
     {
         txtNombre.style.border="solid red medium"
         alertValidacionesTexto.innerHTML=`El <strong>Nombre</strong> no es correcto.<br>`;
         alertValidaciones.style.display="block";
+        isValid= false;
     }
     //valida cantidad
     if(! validarCantidad())
@@ -38,6 +55,32 @@ btnAgregar.addEventListener("click", function(event){
         txtNumber.style.border="solid red medium"
         alertValidacionesTexto.innerHTML+=`El <strong>Cantidad</strong> no es correcto.<br>`;
         alertValidaciones.style.display="block";
+        isValid=false;
+    }
+    if(isValid){
+        contador++;
+        precio = getPrecio();
+        let row =`<tr>
+                    <td>${contador}</td>
+                    <td>${txtNombre.value}</td>
+                    <td>${txtNumber.value}</td>
+                    <td>${precio}</td>
+                </tr>`
+        cuerpoTabla.insertAdjacentHTML("beforeend", row);
+        costoTotal+=precio*Number(txtNumber.value);
+        totalEnProductos+=Number(txtNumber.value);
+        
+        contadorProductos.innerText=contador;
+        productosTotal.innerText=totalEnProductos;
+        precioTotal.innerText="$ "+costoTotal.toFixed(2);
+
+        localStorage.setItem("contador", contador);
+        localStorage.setItem("totalEnProductos", totalEnProductos);
+        localStorage.setItem("costoTotal", costoTotal);
+
+        txtNombre.value="";
+        txtNumber.value="";
+        txtNombre.focus();
     }
 
 
@@ -49,3 +92,27 @@ txtNombre.addEventListener("blur", function(event){
 
     txtNombre.value=txtNombre.value.trim();
 ;})
+//sale del campo
+txtNumber.addEventListener("blur", function(event){
+    //.trim() quia los espacias antes de la primera letra y despues de la ultima letra
+
+    txtNumber.value=txtNumber.value.trim();
+;})
+
+window.addEventListener("load",function(){
+    if (this.localStorage.getItem("contador")!=null){
+        contador=Number(this.localStorage.getItem("contador"));
+        
+    }
+    if (this.localStorage.getItem("totalEnProductos")!=null){
+        totalEnProductos=Number(this.localStorage.getItem("totalEnProductos"));
+        
+    }
+    if (this.localStorage.getItem("costoTotal")!=null){
+        costoTotal=Number(this.localStorage.getItem("costoTotal"));
+    }
+    contadorProductos.innerText=contador;
+    productosTotal.innerText=totalEnProductos;
+    precioTotal.innerText="$ "+costoTotal.toFixed(2);
+
+});
